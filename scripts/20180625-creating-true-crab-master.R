29421241f21033273c517d63ada0506d98b1e129
Grace Crandall <graceac9@uw.edu>
2018-06-25 12:13:40 -0700

    created script for true master

---
+++ scripts/20180625-creating-true-crab-master.R
@@ -0,0 +1,6 @@
+#Script for making a TRUE crab master file. Still missing FRP numbers for the 20180125-Crab-Collection-DATA_DNA-plates.xlsx. This file has morph and _Hematodinium_ blood smear data from the day all the crabs were delivered to the lab (Day 0 and 1)
+
+library(dplyr)
+
+#Need to get all the data in 20180522-master-all-crabs-hemo.csv, 20180522-master-qPCR-all-runs.csv, and 20180514-master-Qubit-RNA-isolation.csv such that each row is an individual crab.
+
