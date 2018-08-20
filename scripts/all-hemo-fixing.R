23cea897ffc952170b99d6502136f8369b9b73d9
sr320 <roberts.sbr@gmail.com>
2018-08-20 13:00:19 -0700

    cleaned up script for hemo table

--- scripts/all-hemo-fixing.R
+++ scripts/all-hemo-fixing.R
@@ -3,16 +3,20 @@
 library(dplyr)
 library(tidyverse)
 
-hemo <- read.csv("data/20180522-all-crabs-hemo.csv")
+hemo_table <- read.csv("data/20180522-all-crabs-hemo.csv")
+
+head(hemo_table)
 
 # Need to change treatment_tank numbers to words. Numbers 1, 2, 3 are COLD; Numbers 4, 5, 6 are WARM; Numbers 7, 8, 9 are AMBIENT. 
 
-hemo <- mutate(hemo, trtmnt_tank = ifelse(treatment_tank == 1 | treatment_tank == 2 | treatment_tank == 3, "cold", ifelse(treatment_tank == 4 | treatment_tank == 5 | treatment_tank == 6, "warm", "ambient")))
+hemo_table <- 
+mutate(hemo_table, trtmnt_tank = ifelse(treatment_tank == 1 | treatment_tank == 2 | treatment_tank == 3, "cold", ifelse(treatment_tank == 4 | treatment_tank == 5 | treatment_tank == 6, "warm", "ambient"))) %>%
+select(year.FRP, FRP, Uniq_ID, sample_day, infection_status, maturity, tube_number, Comments, death, comments, CW, SC, CH, trtmnt_tank) %>%
+arrange(FRP)
 
-colnames(hemo)
 
-# select relevant columns
-newhemo <- hemo %>%
-  select(year.FRP, FRP, Uniq_ID, sample_day, infection_status, maturity, tube_number, Comments, death, comments, CW, SC, CH, trtmnt_tank) %>%
-  arrange(FRP)
+head(hemo_table)
+
+colnames(hemo_table)
+
 
