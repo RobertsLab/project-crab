0f640d4c6fa87ae997efd9301466704c59c78841
Grace Crandall <graceac9@uw.edu>
2018-08-20 11:24:28 -0700

    add to

--- analyses/all-hemo-fixing.R
+++ analyses/all-hemo-fixing.R
@@ -1,1 +1,7 @@
 # Script for making a clean and valuable data sheet to join new data with as project continues
+
+library(dplyr)
+
+hemo <- read.csv("data/20180522-all-crabs-hemo.csv")
+colnames(hemo)
+
