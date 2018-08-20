17363315622bf8c5db66e873a9016c977491bdaa
sr320 <roberts.sbr@gmail.com>
2018-08-20 14:16:15 -0700

    removed year from hemo_table

--- scripts/all-hemo-fixing.R
+++ scripts/all-hemo-fixing.R
@@ -10,8 +10,8 @@ head(hemo_table)
 
 hemo_table <- 
 mutate(hemo_table, trtmnt_tank = ifelse(treatment_tank == 1 | treatment_tank == 2 | treatment_tank == 3, "cold", ifelse(treatment_tank == 4 | treatment_tank == 5 | treatment_tank == 6, "warm", "ambient"))) %>%
-select(year.FRP, FRP, Uniq_ID, trtmnt_tank, sample_day, infection_status, maturity, tube_number) %>%
-arrange(FRP)
+select(FRP, Uniq_ID, trtmnt_tank, sample_day, infection_status, maturity, tube_number) %>%
+arrange(Uniq_ID)
 
 
 head(hemo_table)
