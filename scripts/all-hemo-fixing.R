123beaeefe32657a80ab392562f26e34cddf0d8e
Grace Crandall <graceac9@uw.edu>
2018-10-25 11:09:57 -0700

    add # to top so that stuff doesn't run. already done, don't need it. keeping so that I know what I did to get hemosample_qubit_table.csv

--- scripts/all-hemo-fixing.R
+++ scripts/all-hemo-fixing.R
@@ -3,38 +3,38 @@
 library(tidyverse)
 library(dplyr)
 
-hemo_table <- read.csv("data/20180522-all-crabs-hemo.csv")
+#hemo_table <- read.csv("data/20180522-all-crabs-hemo.csv")
 
-head(hemo_table)
+#head(hemo_table)
 
 # Need to change treatment_tank numbers to words. Numbers 1, 2, 3 are COLD; Numbers 4, 5, 6 are WARM; Numbers 7, 8, 9 are AMBIENT. 
 
-hemo_table <- 
-mutate(hemo_table, trtmnt_tank = ifelse(treatment_tank == 1 | treatment_tank == 2 | treatment_tank == 3, "cold", ifelse(treatment_tank == 4 | treatment_tank == 5 | treatment_tank == 6, "warm", "ambient"))) %>%
-select(FRP, Uniq_ID, trtmnt_tank, sample_day, infection_status, maturity, tube_number) %>%
-arrange(Uniq_ID)
+#hemo_table <- 
+#mutate(hemo_table, trtmnt_tank = ifelse(treatment_tank == 1 | treatment_tank == 2 | treatment_tank == 3, "cold", ifelse(treatment_tank == 4 | treatment_tank == 5 | treatment_tank == 6, "warm", "ambient"))) %>%
+#select(FRP, Uniq_ID, trtmnt_tank, sample_day, infection_status, maturity, tube_number) %>%
+#arrange(Uniq_ID)
 
 
-head(hemo_table)
+#head(hemo_table)
 
-colnames(hemo_table)
+#colnames(hemo_table)
 
-write.csv(hemo_table, "analyses/hemo_table.csv")
+#write.csv(hemo_table, "analyses/hemo_table.csv")
 
-sample_table <- read.csv("analyses/sample_table.csv")
+#sample_table <- read.csv("analyses/sample_table.csv")
 
 # Cleaning up qubit file
-qubit <- read.csv("data/20180817-Qubit-results.csv") %>%
-  select(tube_number, Test_Date, Original_sample_conc_ng.ul, total_sample_vol_ul, total_yield_ng) 
+#qubit <- read.csv("data/20180817-Qubit-results.csv") %>%
+  #select(tube_number, Test_Date, Original_sample_conc_ng.ul, total_sample_vol_ul, total_yield_ng) 
 #add column for extraction method and that they weren't lyophilized
-qubit$extraciton_method <- "RNAzol"
-qubit$lyophilized_y_n <- "n"
+#qubit$extraciton_method <- "RNAzol"
+#qubit$lyophilized_y_n <- "n"
 
 
-sample_qubit <- left_join(sample_table, qubit, by = "tube_number")
+#sample_qubit <- left_join(sample_table, qubit, by = "tube_number")
 
 
-write.csv(sample_qubit, "analyses/hemosample_qubit_table.csv")
+#write.csv(sample_qubit, "analyses/hemosample_qubit_table.csv")
 
 #samsqubit <- read.csv("/Users/graciecrandall/Desktop/QubitData_from_sam_test1-3_spaces.csv", header = TRUE, fileEncoding = "latin1")
 #latin1 deals with µ
