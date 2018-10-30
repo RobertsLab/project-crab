aa0601db6759a63209692e1ab0810296c23ef426
Grace Crandall <graceac9@uw.edu>
2018-10-30 13:11:33 -0700

    add note

--- scripts/hemo-qubit_master.R
+++ scripts/hemo-qubit_master.R
@@ -77,4 +77,4 @@ colnames(qubitnew)
 qubitnew2 <- subset(qubitnew, select = c(tube_number, Test_Date, Original_sample_conc_ng.ul, total_sample_vol_ul, total_yield_ng, extraction_method, lyophilized_y_n))
 
 #check subset qubitnew2 file visually before concatenating to qubitresults
-
+# figure out how to append the qubitnew2 to qubitresults
