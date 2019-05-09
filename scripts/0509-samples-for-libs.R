8f95500019075d917e9cee013af325cf3a7d0bda
Grace Crandall <graceac9@uw.edu>
2019-05-09 14:13:59 -0700

    write out csv of qubit results for samples for library save script

--- scripts/0509-samples-for-libs.R
+++ scripts/0509-samples-for-libs.R
@@ -1,5 +1,5 @@
 # Script to join qubit results from Sam's extractions using the ______ kit on _____
-
+library(tidyverse)
 library(dplyr)
 newres <- read.csv("data/Qubit_data/20190430_qubit_RNA_crab - QubitData_2019-04-30_14-41-32.csv")
 
@@ -30,3 +30,15 @@ system("pwd")
 
 #QubitData_2018-10-31_17-54-22.csv
 system("iconv -f utf-8 -t utf-8 -c analyses/qubit_results-for-libraries.csv | sed 's/(L)/(uL)/g' | sed 's/ng\\/L/ng\\/uL/g' > analyses/qubit_results-for-libraries.csv_UTF8.csv")                   
+
+
+
+#=======================================================
+#JOIN THE NEW RESULTS CSV WITH THE CRAB SAMPLES BY SAMPLE_ID
+
+#read in the hemo_sample_qubit_table.csv
+hemo <- read.csv("analyses/hemo_table.csv")
+
+qubforlib <- left_join(newqub, hemo, by = "Uniq_ID")
+
+write.csv(qubforlib, "analyses/hemo_qub-for-libs.csv")
